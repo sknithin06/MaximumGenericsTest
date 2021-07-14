e7538b252dfb4cfaf2fa6413b6e0da98864a4a80
sknithin06 <nithinsk939@gmail.com>
2021-07-14 21:50:39 +0530

    Enter value in float

--- MaximumValue.java
+++ MaximumValue.java
@@ -27,11 +27,37 @@ public class MaximumValue {
 			System.out.println("Maximum value is a:"+max);
 		}
 	}
-	
+	 static void numMaximumFloat() {
+			Scanner sc = new Scanner(System.in);
+			System.out.print("Enter your First Number:");  
+			Float a = sc.nextFloat();
+			System.out.print("Enter your Second Number:");
+			Float b = sc.nextFloat();
+			System.out.print("Enter your Third Number:");
+			Float c = sc.nextFloat();
+
+			if (b.compareTo(a) > 0) {
+				if (b.compareTo(c) > 0) {
+					Float max = b;
+					System.out.println("Maximum value is b:"+max);
+				}
+			}
+			if (c.compareTo(a) > 0) {
+				if (c.compareTo(b) > 0) {
+					Float max = c;
+					System.out.println("Maximum value is c:"+max);
+				}
+			}
+			else {
+				 Float max = a;
+				System.out.println("Maximum value is a:"+max);
+			}
+		}
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generic Problem");
 		
 		MaximumValue  value = new MaximumValue ();
 		value.numMaximumInt();
+		value.numMaximumFloat();
 	}
 }
