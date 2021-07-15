6acf426be767c2228b58bb348e0ae5d4b02ab111
sknithin06 <nithinsk939@gmail.com>
2021-07-15 21:32:11 +0530

    Enter value in one method

--- MaximumValue.java
+++ MaximumValue.java
@@ -1,7 +1,35 @@
 package bridgelabz;
 import java.util.Scanner;
-public class MaximumValue {
-	 static void numMaximumInt() {
+public class MaximumValue <T extends Comparable<T>> {
+	T p, q, r;
+
+	public T maximum() {
+		return MaximumValue.maximum(p, q, r);
+	}
+	 public static <T extends Comparable<T>> T maximum(T p, T q, T r) {
+		if (q.compareTo(p) > 0) {
+			if (q.compareTo(r) > 0) {
+				 T max = q;
+				System.out.println("Maximum value :"+max);
+				return max;
+			}
+			return q;
+		}
+		if (r.compareTo(p) > 0) {
+			if (r.compareTo(q) > 0) {
+				T max = r;
+				System.out.println("Maximum value :"+max);
+				return max;
+			}
+			return r;
+		}
+		else {
+			 T max = p;
+			System.out.println("Maximum value :"+max);
+			return max;
+		}
+	}
+	static void numMaximumInt() {
 		Scanner sc = new Scanner(System.in);
 		System.out.print("Enter your First Number:");  
 		Integer a = sc.nextInt();
@@ -9,7 +37,6 @@ public class MaximumValue {
 		Integer b = sc.nextInt();
 		System.out.print("Enter your Third Number:");
 		Integer c = sc.nextInt();
-
 		if (b.compareTo(a) > 0) {
 			if (b.compareTo(c) > 0) {
 				Integer max = b;
@@ -27,7 +54,7 @@ public class MaximumValue {
 			System.out.println("Maximum value is a:"+max);
 		}
 	}
-	 static void numMaximumFloat() {
+	static void numMaximumFloat() {
 			Scanner sc = new Scanner(System.in);
 			System.out.print("Enter your First Number:");  
 			Float a = sc.nextFloat();
@@ -35,7 +62,6 @@ public class MaximumValue {
 			Float b = sc.nextFloat();
 			System.out.print("Enter your Third Number:");
 			Float c = sc.nextFloat();
-
 			if (b.compareTo(a) > 0) {
 				if (b.compareTo(c) > 0) {
 					Float max = b;
@@ -79,12 +105,40 @@ public class MaximumValue {
 			System.out.println("Maximum value is a:"+max);
 		}
 	} 
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Generic Problem");
 		
-		MaximumValue  value = new MaximumValue ();
-		value.numMaximumInt();
-		value.numMaximumFloat();
-		value.numMaximumString();
+		numMaximumInt();
+		numMaximumFloat();
+		numMaximumString();
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your First Number:");  
+		Float pFl = sc.nextFloat();
+		System.out.print("Enter your Second Number:");
+		Float qFl = sc.nextFloat();
+		System.out.print("Enter your Third Number:");
+		Float rFl = sc.nextFloat();
+		
+		System.out.print("Enter your First Number:");  
+		Integer pInt = sc.nextInt();
+		System.out.print("Enter your Second Number:");
+		Integer qInt = sc.nextInt();
+		System.out.print("Enter your Third Number:");
+		Integer rInt = sc.nextInt();
+		
+		System.out.print("Enter your First Number:");  
+		String pStr = sc.nextLine();
+		System.out.print("Enter your Second Number:");
+		String qStr = sc.nextLine();
+		System.out.print("Enter your Third Number:");
+		String rStr = sc.nextLine();
+		
+		maximum(pInt, qInt, rInt);
+		maximum(pFl, qFl, rFl);
+		maximum(pStr, qStr, rStr);
+	
+		
 	}
 }
